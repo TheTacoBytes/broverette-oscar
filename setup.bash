3179a7f44200e90ef693502584a96e950a04fb8d
Humberto Ruiz-Ochoa <thetacobytes@gmail.com>
2024-11-06 21:14:09 -0500

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -13,5 +13,9 @@ source /opt/ros/humble/setup.bash
 source broverette_oscar_ws/install/setup.bash
 
 ##
+#Multi machine 
+EXPORT ROS_DOMAIN_ID=1
+
+##
 # add neural_net folder to PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:$(pwd)/neural_net
